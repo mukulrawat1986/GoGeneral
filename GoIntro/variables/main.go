0df8d9881d3c30193c1295e3bd9db03d2b8440b7
Mukul Rawat <mukulsrawat@gmail.com>
2020-12-15 23:10:12 +0530

    example of using iota

--- GoIntro/variables/main.go
+++ GoIntro/variables/main.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
-var (
-	message = "The answer to life is %d\n"
-	answer = 42
+const (
+	message = "The answer to life is %d %d\n"
+	answer1 = iota
+	answer2
 )
 
 func main() {
-	fmt.Printf(message, answer)
+	fmt.Printf(message, answer1, answer2)
 }
