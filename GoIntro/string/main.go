ed1534fb933a46e8ae6c6d2f902ab05f51d71f3e
Mukul Rawat <mukulsrawat@gmail.com>
2020-12-17 11:08:53 +0530

    getting length of string

--- GoIntro/string/main.go
+++ GoIntro/string/main.go
@@ -12,4 +12,12 @@ func main() {
 	for i, r := range atoz {
 		fmt.Printf("%d %c\n", i, r)
 	}
+
+	// only using indices when going through string rune by rune
+	for i := range atoz {
+		fmt.Printf("%d ", i)
+	}
+
+	// get length of string
+	fmt.Printf("\nLength of string is: %d\n", len(atoz))
 }
