b1c59518134f98f1c9754c6c069d02d90ce1140d
Mukul Rawat <mukulsrawat@gmail.com>
2020-12-17 15:44:01 +0530

    echo3 prints its command line arguments

---
+++ gopl/ch1/echo3/main.go
@@ -0,0 +1,12 @@
+// echo3 prints its command line arguments in a cost efficient way
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
