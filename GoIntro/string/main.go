ae1377929e203b9a732543cb402df18edc71823d
Mukul Rawat <mukulsrawat@gmail.com>
2020-12-17 11:10:28 +0530

    using backquotes

--- GoIntro/string/main.go
+++ GoIntro/string/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	atoz := "the quick brown fox jumps over the lazy dog\n"
+	atoz := `the quick "brown fox" jumps over the lazy dog\n`
 
 	// take substrings
 	fmt.Printf("%s\n", atoz[0:9])
