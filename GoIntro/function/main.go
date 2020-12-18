a532c106e34fa2e5cbfc51d5662d37670cd3f446
Mukul Rawat <mukulsrawat@gmail.com>
2020-12-18 12:09:22 +0530

    returning from the function

--- GoIntro/function/main.go
+++ GoIntro/function/main.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
-func printer(msg, msg2 string, repeat int) {
-	for repeat > 0 {
-		fmt.Printf("%s", msg)
-		fmt.Printf("%s\n", msg2)
-		repeat--
-	}
+func printer(msg string) error {
+	_, err := fmt.Printf("%s\n", msg)
+	return err
 }
 
 func main() {
-	printer("Hello, ", "World!", 5)
+	if err := printer("Hello, World!"); err != nil {
+
+	}
 }
