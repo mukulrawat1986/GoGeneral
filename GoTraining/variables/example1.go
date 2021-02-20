8068751398854e441a0c0c9ce1d7b4185f8781f7
Mukul Rawat <mukulsrawat@gmail.com>
2021-02-20 18:00:54 +0530

    decalre variables that start in zero value state

---
+++ GoTraining/variables/example1.go
@@ -0,0 +1,19 @@
+// Sample program to show how to declare variables
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Declare variables that are set to their zero value
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Printf("var a int \t %T [%v]\n", a, a)
+	fmt.Printf("var b string \t %T [%v]\n", b, b)
+	fmt.Printf("var c float64 \t %T [%v]\n", c, c)
+	fmt.Printf("var d bool \t %T [%v]\n", d, d)
+}
