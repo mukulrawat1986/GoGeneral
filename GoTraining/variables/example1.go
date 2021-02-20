a85c9d052f58f6d753eca43c0c85530666acc770
Mukul Rawat <mukulsrawat@gmail.com>
2021-02-20 18:04:41 +0530

    declare variables and initialize using short variable declaration operator

--- GoTraining/variables/example1.go
+++ GoTraining/variables/example1.go
@@ -16,4 +16,16 @@ func main() {
 	fmt.Printf("var b string \t %T [%v]\n", b, b)
 	fmt.Printf("var c float64 \t %T [%v]\n", c, c)
 	fmt.Printf("var d bool \t %T [%v]\n", d, d)
+
+	// Declare variables and initialize.
+	// Using the short variable declaration operator.
+	aa := 10
+	bb := "hello"
+	cc := 3.14159
+	dd := true
+
+	fmt.Printf("aa := 10 \t %T [%v]\n", aa, aa)
+	fmt.Printf("bb := \"hello\" \t %T [%v]\n", bb, bb)
+	fmt.Printf("cc := 3.14159 \t %T [%v]\n", cc, cc)
+	fmt.Printf("dd := true \t %T [%v]\n", dd, dd)
 }
