92c356d81f7abe680fed466708adeca054b0a344
Mukul Rawat <mukulsrawat@gmail.com>
2021-02-20 19:43:38 +0530

    declare variables and print the values

---
+++ GoTraining/variables/exercise1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Declare variables that are set to their zero value
+	var a int
+	var b string
+	var c bool
+
+	// Display the value of the variables
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	// Declare variables and initialize.
+	// Using the short variable declaration operator.
+	aa := 12
+	bb := "hello"
+	cc := true
+
+	// Display the value of those variables
+	fmt.Println(aa)
+	fmt.Println(bb)
+	fmt.Println(cc)
+}
