2a69dda7b5ce5d1d968eb8621670697fce5cf6e8
Mukul Rawat <mukulsrawat@gmail.com>
2021-03-03 01:12:13 +0530

    add support for language and refactor

--- learn-go-with-tests/hello/hello.go
+++ learn-go-with-tests/hello/hello.go
@@ -4,18 +4,23 @@ import "fmt"
 
 const (
 	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
 )
 
 // Hello function returns a string
-func Hello(name string) string {
+func Hello(name, language string) string {
 
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
