d278a1e48becc4bb59b75bc5d39e5eb48454ba16
Mukul Rawat <mukulsrawat@gmail.com>
2021-03-02 23:09:50 +0530

    remove domain code from side effect

--- learn-go-with-tests/hello/hello.go
+++ learn-go-with-tests/hello/hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// Hello function returns a string
+func Hello() string {
+	return "Hello, World!"
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(Hello())
 }
